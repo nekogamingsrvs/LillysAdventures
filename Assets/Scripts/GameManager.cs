2093f8448c1d16d8f43f584177af9af072081055
thakyZ <thakyz@outlook.com>
2015-10-15 19:53:35 -0700

    - Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,29 +12,13 @@ public class GameManager : MonoBehaviour
 
 	// Use this for initialization
 	void Start()
-    {
+	{
 		debugText.gameObject.SetActive(isDebugActive);
 		levelBoundries = new Rect();
 		levelBoundries.xMin = 0;
-		levelBoundries.xMax = level.GetComponentInChildren<Collider2D>().bounds.size.x;
-
-		if (level.GetComponentInChildren<Collider2D>().bounds.max.y > 0)
-		{
-			levelBoundries.yMax = level.GetComponentInChildren<Collider2D>().bounds.max.y;
-		}
-		else
-		{
-			levelBoundries.yMax = 0;
-		}
-
-		if (level.GetComponentInChildren<Collider2D>().bounds.min.y < -10)
-		{
-			levelBoundries.yMin = level.GetComponentInChildren<Collider2D>().bounds.max.y;
-		}
-		else
-		{
-			levelBoundries.yMin = -10;
-		}
+		levelBoundries.xMax = 100;
+		levelBoundries.yMin = -10;
+		levelBoundries.yMax = 0;
 	}
 	
 	// Update is called once per frame
