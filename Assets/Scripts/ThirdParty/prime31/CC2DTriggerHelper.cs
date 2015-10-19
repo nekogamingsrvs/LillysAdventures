0b3046d3e8dd20dc5166e6927d2e441698632b97
thakyZ <thakyz@outlook.com>
2015-10-19 08:23:50 -0700

    - Update 10/19/2015     - Updated .gitignore     - Removed the .sln and .csproj files.     - Updated and removed sprites.     - Moved some folders.     - Updated code.     - Added CnInputControls library for android controls.

---
+++ Assets/Scripts/ThirdParty/prime31/CC2DTriggerHelper.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// this script just captures the OnTrigger* messages and passes them on to the CharacterController2D
+/// </summary>
+public class CC2DTriggerHelper : MonoBehaviour
+{
+	/// <summary>
+	/// The parent Character Controller 2D to use in the trigger helper.
+	/// </summary>
+	private CharacterController2D _parentCharacterController;
+
+	/// <summary>
+	/// Sets the parent Character Controller 2D used in the trigger helper.
+	/// </summary>
+	/// <param name="parentCharacterController">The parent Character Controller.</param>
+	public void setParentCharacterController(CharacterController2D parentCharacterController)
+	{
+		_parentCharacterController = parentCharacterController;
+	}
+	
+	#region MonoBehavior
+	/// <summary>
+	/// Gets if the trigger entered a 2D collision.
+	/// </summary>
+	/// <param name="col">The collider to check.</param>
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.isTrigger)
+		{
+			_parentCharacterController.OnTriggerEnter2D(col);
+		}
+	}
+
+	/// <summary>
+	/// Gets if the trigger stayed on a 2D collision.
+	/// </summary>
+	/// <param name="col">The collider to check.</param>
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.isTrigger)
+		{
+			_parentCharacterController.OnTriggerStay2D(col);
+		}
+	}
+
+	/// <summary>
+	/// Gets if the trigger exited a 2D collision.
+	/// </summary>
+	/// <param name="col">The collider to check.</param>
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.isTrigger)
+			_parentCharacterController.OnTriggerExit2D(col);
+	}
+	#endregion
+}
