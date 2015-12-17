7cd6895ab053660c767ff4059ee3c2584b6e6838
thakyZ <thakyz@outlook.com>
2015-12-17 06:28:04 -0800

    Fixed some formatting and spelling issues

--- Assets/Tiled2Unity/Scripts/Editor/ICustomTiledImporter.cs
+++ Assets/Tiled2Unity/Scripts/Editor/ICustomTiledImporter.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 namespace Tiled2Unity
 {
 	interface ICustomTiledImporter
-    {
-        // A game object within the prefab has some custom properites assigned through Tiled that are not consumed by Tiled2Unity
-        // This callback gives customized importers a chance to react to such properites.
-        void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> customProperties);
+	{
+		// A game object within the prefab has some custom properties assigned through Tiled that are not consumed by Tiled2Unity
+		// This callback gives customized importers a chance to react to such properties.
+		void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> customProperties);
 
-        // Called just before the prefab is saved to the asset database
-        // A last chance opporunity to modify it through script
-        void CustomizePrefab(GameObject prefab);
-    }
+		// Called just before the prefab is saved to the asset database
+		// A last chance opportunity to modify it through script
+		void CustomizePrefab(GameObject prefab);
+	}
 }
 
 // Examples
--- Assets/Tiled2Unity/Scripts/Editor/ImportTiled2Unity.Texture.cs
+++ Assets/Tiled2Unity/Scripts/Editor/ImportTiled2Unity.Texture.cs
@@ -5,13 +5,13 @@ namespace Tiled2Unity
 {
 	// Handled a texture being imported
 	partial class ImportTiled2Unity
-    {
-        public void TextureImported(string texturePath)
-        {
-            // This is fix up method due to materials and textures, under some conditions, being imported out of order
-            Texture2D texture2d = AssetDatabase.LoadAssetAtPath(texturePath, typeof(Texture2D)) as Texture2D;
-            Material material = AssetDatabase.LoadAssetAtPath(GetMaterialAssetPath(texturePath), typeof(Material)) as Material;
-            material.SetTexture("_MainTex", texture2d);
-        }
-    }
+	{
+		public void TextureImported(string texturePath)
+		{
+			// This is fix up method due to materials and textures, under some conditions, being imported out of order
+			Texture2D texture2d = AssetDatabase.LoadAssetAtPath(texturePath, typeof(Texture2D)) as Texture2D;
+			Material material = AssetDatabase.LoadAssetAtPath(GetMaterialAssetPath(texturePath), typeof(Material)) as Material;
+			material.SetTexture("_MainTex", texture2d);
+		}
+	}
 }
