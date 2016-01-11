799086179270304a50fbb4d8d6c77ba5db6968a6
thakyZ <thakyz@outlook.com>
2016-01-11 11:11:03 -0800

    Updated PlayerController2D class.
    This should now fix ladders, since they were broken.
    Also changed around some variables names.

--- Assets/Scripts/Player/TriggerTest.cs
+++ Assets/Scripts/Player/TriggerTest.cs
@@ -2,51 +2,53 @@
 using System.Linq;
 using UnityEngine;
 
-// Helper class for collecting trigger collisions
-// This is needed because OnTriggerExit2D is not reliable
-// So, we have to poll for trigger collision status each frame
-// Note: This is not reliable either. Don't use unless you don't mind skipping the odd trigger on per-frame basis
-// (It's okay to use for a one-shot response to being in a trigger)
-class TriggerTest : MonoBehaviour
+namespace VoidInc
 {
-	private List<Collider2D> triggerColliders = new List<Collider2D>();
-
-	public T GetTriggerCollider<T>() where T : Collider2D
+	// Helper class for collecting trigger collisions
+	// This is needed because OnTriggerExit2D is not reliable
+	// So, we have to poll for trigger collision status each frame
+	// Note: This is not reliable either. Don't use unless you don't mind skipping the odd trigger on per-frame basis
+	// (It's okay to use for a one-shot response to being in a trigger)
+	class TriggerTest : MonoBehaviour
 	{
-		var colliders = this.triggerColliders.OfType<T>();
-		if (colliders.Count() > 0)
+		private List<Collider2D> triggerColliders = new List<Collider2D>();
+
+		public T GetTriggerCollider<T>() where T : Collider2D
 		{
-			return colliders.First();
+			var colliders = this.triggerColliders.OfType<T>();
+			if (colliders.Count() > 0)
+			{
+				return colliders.First();
+			}
+			return null;
 		}
-		return null;
-	}
 
-	public bool IsInTrigger()
-	{
-		return this.triggerColliders.Count() > 0;
-	}
-
-	private void OnTriggerEnter2D(Collider2D collider)
-	{
-		if (!this.triggerColliders.Contains(collider))
+		public bool IsInTrigger()
 		{
-			this.triggerColliders.Add(collider);
+			return this.triggerColliders.Count() > 0;
 		}
-	}
 
-	private void OnTriggerStay2D(Collider2D collider)
-	{
-		if (!this.triggerColliders.Contains(collider))
+		private void OnTriggerEnter2D(Collider2D collider)
 		{
-			this.triggerColliders.Add(collider);
+			if (!this.triggerColliders.Contains(collider))
+			{
+				this.triggerColliders.Add(collider);
+			}
 		}
-	}
 
-	private void LateUpdate()
-	{
-		// Clear the triggers at the end of every frame
-		// They will get picked up again in a OnTriggerStay2D call
-		this.triggerColliders.Clear();
+		private void OnTriggerStay2D(Collider2D collider)
+		{
+			if (!this.triggerColliders.Contains(collider))
+			{
+				this.triggerColliders.Add(collider);
+			}
+		}
+
+		private void LateUpdate()
+		{
+			// Clear the triggers at the end of every frame
+			// They will get picked up again in a OnTriggerStay2D call
+			this.triggerColliders.Clear();
+		}
 	}
 }
-
