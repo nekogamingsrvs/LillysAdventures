74a50f93f12942a3043e68d6c4a887e160e4c478
thakyZ <thakyz@outlook.com>
2016-01-12 11:07:01 -0800

    Trying to fix the menu.
    Also added it so that the skybox changes color (still wip)

--- Assets/Scripts/Managers/Game/DayCycleManager.cs
+++ Assets/Scripts/Managers/Game/DayCycleManager.cs
@@ -8,16 +8,18 @@ namespace VoidInc
 		/// Time of day in minutes.
 		/// </summary>
 		public float DayLength = 1024.0f;
-
 		/// <summary>
 		/// The current time of day.
 		/// </summary>
 		public float CurrentTime = 0.5f;
-
 		/// <summary>
 		/// The sun's initial intensity.
 		/// </summary>
 		private float _SunInitialIntensity = 1.0f;
+
+		public float intensity;
+
+		public float CurrentTimeDelta;
 
 		// Update is called once per frame
 		void Update()
@@ -25,6 +27,8 @@ namespace VoidInc
 			UpdateSun();
 
 			CurrentTime += (Time.deltaTime / (DayLength)) * 5f;
+
+			CurrentTimeDelta = Mathf.Repeat(CurrentTime + 0.75f, 1.0f);
 
 			if (CurrentTime >= 1)
 			{
@@ -48,8 +52,12 @@ namespace VoidInc
 			{
 				intensityMultiplier = Mathf.Clamp((1 - ((CurrentTime - 0.73f) * (1 / 0.02f))), 0.25f, 1.0f);
 			}
+			
+			intensity = _SunInitialIntensity * intensityMultiplier;
 
-			gameObject.GetComponent<Light>().intensity = _SunInitialIntensity * intensityMultiplier;
+			gameObject.GetComponent<Light>().intensity = intensity;
+
+			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().backgroundColor = Color.Lerp(new Color(0.6705882352941176f, 0.9764705882352941f, 1.00f), Color.black, CurrentTimeDelta);
 		}
 	}
 }
