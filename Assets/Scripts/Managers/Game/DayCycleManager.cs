e3fe9edbe9358c309f879eb99dea23c165dafe01
thakyZ <thakyz@outlook.com>
2016-01-12 11:36:35 -0800

    Fixed the Menu and made the skybox change color

--- Assets/Scripts/Managers/Game/DayCycleManager.cs
+++ Assets/Scripts/Managers/Game/DayCycleManager.cs
@@ -17,19 +17,13 @@ namespace VoidInc
 		/// </summary>
 		private float _SunInitialIntensity = 1.0f;
 
-		public float intensity;
-
-		public float CurrentTimeDelta;
-
 		// Update is called once per frame
 		void Update()
 		{
 			UpdateSun();
 
 			CurrentTime += (Time.deltaTime / (DayLength)) * 5f;
-
-			CurrentTimeDelta = Mathf.Repeat(CurrentTime + 0.75f, 1.0f);
-
+			
 			if (CurrentTime >= 1)
 			{
 				CurrentTime = 0;
@@ -39,25 +33,27 @@ namespace VoidInc
 		void UpdateSun()
 		{
 			float intensityMultiplier = 1.0f;
+			float time = 1.0f;
 
 			if (CurrentTime <= 0.10f || CurrentTime >= 0.80f)
 			{
 				intensityMultiplier = 0.25f;
-			}
+            }
 			else if (CurrentTime <= 0.30f)
 			{
 				intensityMultiplier = Mathf.Clamp((CurrentTime - 0.23f) * (1 / 0.02f), 0.25f, 1.0f);
+				time = Mathf.Clamp((CurrentTime - 0.23f) * (1 / 0.02f), 0.0f, 1.0f);
+				GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().backgroundColor = Color.Lerp(Color.black, new Color(0.6705882352941176f, 0.9764705882352941f, 1.00f), time);
 			}
 			else if (CurrentTime >= 0.60f)
 			{
 				intensityMultiplier = Mathf.Clamp((1 - ((CurrentTime - 0.73f) * (1 / 0.02f))), 0.25f, 1.0f);
+				time = Mathf.Clamp((1 - ((CurrentTime - 0.73f) * (1 / 0.02f))), 0.0f, 1.0f);
+				GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().backgroundColor = Color.Lerp(Color.black, new Color(0.6705882352941176f, 0.9764705882352941f, 1.00f), time);
 			}
-			
-			intensity = _SunInitialIntensity * intensityMultiplier;
 
-			gameObject.GetComponent<Light>().intensity = intensity;
+			gameObject.GetComponent<Light>().intensity = _SunInitialIntensity * intensityMultiplier;
 
-			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().backgroundColor = Color.Lerp(new Color(0.6705882352941176f, 0.9764705882352941f, 1.00f), Color.black, CurrentTimeDelta);
 		}
 	}
 }
